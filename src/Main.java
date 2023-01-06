5d50803b787616e491e491ba9fc0917b2bf32b63
darthjotinha <jopesame@gmail.com>
2023-01-06 11:11:02 -0300

    Commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
+import dev.jota.Curso;
+import dev.jota.Mentoria;
+
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Java");
+        curso1.setDescricao("Curso sobre Java");
+        curso1.setCargaHoraria(8);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("JavaScript");
+        curso2.setDescricao("Curso sobre JavaScript");
+        curso2.setCargaHoraria(4);
+
+        Mentoria mentoria1 = new Mentoria();
+        mentoria1.setTitulo("Mentoria de Java");
+        mentoria1.setDescricao("Descrição Mentoria de Java");
+        mentoria1.setData(LocalDate.now());
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria1);
+
     }
 }
